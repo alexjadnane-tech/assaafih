20861a18aed3c3e81f6be6362b3c6d8bd3c44ba3
alexjadnane-tech <alexj.adnane@gmail.com>
2025-12-09 13:25:25 +0100

    Update payrexx.js

--- api/payrexx.js
+++ api/payrexx.js
@@ -6,7 +6,20 @@ export default function handler(req, res) {
   const instance = process.env.PAYREXX_INSTANCE;
   const apiKey = process.env.PAYREXX_API_KEY;
 
-  const payrexxUrl = `https://${instance}.payrexx.com/checkout?api_key=${apiKey}&amount=700&currency=CHF&description=Carnet+édition+${edition}&success_redirect_url=${baseUrl}/success.html&failed_redirect_url=${baseUrl}/cancel.html&custom_fields[edition]=${edition}`;
+  // --- Construction propre de l'URL avec encoding
+  const params = new URLSearchParams({
+    api_key: apiKey,
+    amount: '700',
+    currency: 'CHF',
+    description: `Carnet édition ${edition}`,
+    success_redirect_url: `${baseUrl}/success.html`,
+    failed_redirect_url: `${baseUrl}/cancel.html`,
+    'custom_fields[edition]': edition
+  });
 
+  const payrexxUrl = `https://${instance}.payrexx.com/checkout?${params.toString()}`;
+
+  // Redirection vers Payrexx
   res.redirect(payrexxUrl);
 }
+
