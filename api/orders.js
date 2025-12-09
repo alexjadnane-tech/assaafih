4aa9624f8c49e42eee29c701953bdffe78129475
alexjadnane-tech <alexj.adnane@gmail.com>
2025-12-10 00:20:28 +0100

    Delete api/orders.js

--- api/orders.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-export default function handler(req, res) {
-  const file = path.join(process.cwd(), 'orders.json');
-  if (!fs.existsSync(file)) return res.json([]);
-  const orders = JSON.parse(fs.readFileSync(file));
-  res.json(orders);
-}
